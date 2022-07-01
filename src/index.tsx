4fa0c54c6c19fbe548f43fa57b4348c0994028a7
Eric Pinheiro <ericfariaspinheiro@gmail.com>
2022-07-01 18:52:17 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<div></div>);
